c36e6aed91d242030e92fe014afe29d0b7a23056
tb <t.bak@selleo.com>
2015-06-28 20:50:50 +0200

    simple ng-model example

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,17 +1,12 @@
-import {Component, View, NgIf} from 'angular2/angular2';
+import {Component, View, NgIf, formDirectives} from 'angular2/angular2';
 
 @Component({
   selector: 'home'
 })
 @View({
   templateUrl: 'components/home/home.html',
-  directives: [NgIf]
+  directives: [NgIf, formDirectives]
 })
 export class Home {
-  name: string = 'World';
-  constructor() {
-    setTimeout(() => {
-      this.name = 'NEW World'
-    }, 2000);
-  }
+  name: string;
 }
