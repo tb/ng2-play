62608fd5a834be494ee5e22fa50f2a74a271e546
tb <t.bak@selleo.com>
2015-07-15 19:51:22 +0200

    upgrade to 2.0.0-alpha.31

--- src/components/users/users.ts
+++ src/components/users/users.ts
@@ -1,5 +1,5 @@
 import {Component, View, NgFor} from 'angular2/angular2';
-import {Http, httpInjectables} from 'angular2/http';
+import {Http} from 'angular2/http';
 
 @Component({
   selector: 'users'
@@ -12,6 +12,8 @@ export class Users {
   users: Array;
 
   constructor(http: Http) {
-    http.get('http://localhost:3002/users.json').map(res => res.json()).subscribe(users => this.users = users);
+    http.get( 'http://localhost:3002/users.json')
+      //.map(res => res.json())
+      //.subscribe(users => this.users = users);
   }
 }
